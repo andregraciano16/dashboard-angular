2ab28e08d29b519e657fd5d621d5cc2f27b83d39
André Graciano <andre.sousa@spread.com.br>
2020-08-01 12:55:22 -0300

    criando modulo de dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { DashboardModule } from './dashboard';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DashboardModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
